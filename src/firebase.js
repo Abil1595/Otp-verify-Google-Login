11b41096ed52710426e5f24fd0dc0fa8faa02558
Electric & Tech World <98805202+Abil1595@users.noreply.github.com>
2024-06-08 12:29:26 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 
 var firebaseConfig = {
    /*  enter your firebase content here */
-    apiKey: "AIzaSyAitXtadiMITxrXmgt-D6MrIALYRqDJQUk",
-    authDomain: "otp-auth-1f9e8.firebaseapp.com",  
-    projectId: "otp-auth-1f9e8",
-    storageBucket: "otp-auth-1f9e8.appspot.com",
-    messagingSenderId: "970939805652",
-    appId: "1:970939805652:web:085364d167401439203041"
+    apiKey: " ",
+    authDomain: " ",  
+    projectId: " ",
+    storageBucket: " ",
+    messagingSenderId: " ",
+    appId: " "
   };
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const auth = firebaseApp.auth();
