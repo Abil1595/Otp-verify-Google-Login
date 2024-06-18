2fb07a5b87c0ca296bf599f5aa07a8eb584dd76b
Electric & Tech World <98805202+Abil1595@users.noreply.github.com>
2024-06-18 17:57:50 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
-var firebaseConfig = {
-   /*  enter your firebase content here */
-    apiKey: " ",
-    authDomain: " ",  
-    projectId: " ",
-    storageBucket: " ",
-    messagingSenderId: " ",
-    appId: " "
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyAitXtadiMITxrXmgt-D6MrIALYRqDJQUk",
+  authDomain: "otp-auth-1f9e8.firebaseapp.com",
+  projectId: "otp-auth-1f9e8",
+  storageBucket: "otp-auth-1f9e8.appspot.com",
+  messagingSenderId: "970939805652",
+  appId: "1:970939805652:web:085364d167401439203041"
+};
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const auth = firebaseApp.auth();
   const provider = new firebase.auth.GoogleAuthProvider();
